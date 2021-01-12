fe4a0167d37d7b327be538d11319502aa9c1b8ed
MeryPopins <72820787+MeryPopins312@users.noreply.github.com>
2021-01-12 17:01:20 +0600

    Update MainActivity.java

--- app/src/main/java/com/example/myhw1/MainActivity.java
+++ app/src/main/java/com/example/myhw1/MainActivity.java
@@ -5,10 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+     Button btnSing;
+    EditText password,sing;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+     private void Init(){
+        sing = findViewById(R.id.login);
+        password = findViewById(R.id.password);
+
+        findViewById(R.id.btnSing).setOnClickListener(v -> {
+            if (sing.getText().toString().isEmpty() || password.getText().toString().isEmpty()) {
+                Toast.makeText(MainActivity.this,"Введите логин или пароль" , Toast.LENGTH_SHORT).show();
+            } else if (password.length() > 6)
+                Toast.makeText(MainActivity.this, "Успешно", Toast.LENGTH_SHORT).show();
+            else {
+                Toast.makeText(MainActivity.this, "Пароль должен содержать более 6 символов", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
